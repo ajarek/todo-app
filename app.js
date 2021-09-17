bc71a7d585afcd1ca13ef915684b0dd63b21ce34
ajarek <ajarek@poczta.onet.pl>
2021-09-17 16:35:36 +0200

    commit initial

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const {port,database} = require('./config')
+
+const app = express()
+
+mongoose.connect(database)
+
+//middleware
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+//routes
+app.use(require('./routes/index'))
+app.use(require('./routes/todo'))
+
+app.listen(port, () => {
+    console.log('Server started listening:' + port);
+})
